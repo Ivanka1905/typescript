4b35652237576f4a9cd218dc11fba9c64bfe933c
Ivanka Nagornyuk <IvNagornyuk@gmail.com>
2023-08-18 22:34:45 +0300

    up

--- app.js
+++ app.js
@@ -1,9 +1,76 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function add(num1, num2) {
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+// button.addEventListener("click", function() {
+//   console.log(add(+input1.value, +input2.value));
+// });
+var age;
+var name1;
+var toggle;
+var empty;
+var notInitialize;
+function callback(a) {
+    return 100 + a;
+}
+age = 50;
+name1 = "Max";
+toggle = true;
+empty = null;
+notInitialize = undefined;
+callback(10);
+// ---2
+var anything;
+anything = -20;
+anything = 'Text';
+anything = {};
+// ---3
+var some;
+some = 'Text';
+var str;
+if (typeof some === "string") {
+    str = some;
+}
+// ---4
+var person;
+person = ['Max', 21];
+// ---5
+var Task;
+(function (Task) {
+    Task[Task["LOADING"] = 0] = "LOADING";
+    Task[Task["READY"] = 1] = "READY";
+})(Task || (Task = {}));
+// ---6
+var union;
+var literal;
+// ---7
+function showMessage(message) {
+    console.log(message);
+}
+;
+function calc(num1, num2) {
     return num1 + num2;
 }
-button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
-});
+;
+function customError() {
+    throw new Error('Error');
+}
+;
+var page1 = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+        createAt: new Date('2021-01-01'),
+        updateAt: new Date('2021-05-01'),
+    }
+};
+var page2 = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close',
+};
--- app.ts
+++ app.ts
@@ -1,11 +1,96 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+
+// button.addEventListener("click", function() {
+//   console.log(add(+input1.value, +input2.value));
+// });
+let age: number;
+let name1: string;
+let toggle: boolean;
+let empty: null;
+let notInitialize: undefined;
+function callback(a: number): number {
+  return 100 + a;
 }
 
-button.addEventListener("click", function() {
-  console.log(add(+input1.value, +input2.value));
-});
+age = 50;
+name1 = "Max";
+toggle = true;
+empty = null;
+notInitialize = undefined;
+callback(10);
+// ---2
+let anything: any;
+anything = -20;
+anything = 'Text';
+anything = {};
+// ---3
+let some:unknown;
+some = 'Text';
+
+let str: string;
+if (typeof some === "string") {
+  str = some;
+}
+// ---4
+let person: [string, number]
+person = ['Max', 21];
+// ---5
+enum Task { LOADING, READY };
+const page = {
+  load: Task.READY
+}
+if (page.load === Task.LOADING) {
+  console.log("Сторінка завантажується")
+}
+if (page.load === Task.READY) {
+  console.log("Сторінка завантажена")
+}
+// ---6
+let union: string | number;
+let literal: 'enable' | 'disable';
+
+// ---7
+function showMessage(message: string): void {
+  console.log(message);
+};
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+};
+
+function customError(): never {
+  throw new Error('Error');
+};
+// ---8
+type Page = {
+  title: string,
+  likes: number,
+  accounts: string[],
+  status: 'open' | 'close',
+  details?: {
+    createAt: Date,
+    updateAt: Date,
+  }
+}
+const page1: Page = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: new Date('2021-01-01'),
+    updateAt: new Date('2021-05-01'),
+  }
+}
+
+const page2: Page = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+}
